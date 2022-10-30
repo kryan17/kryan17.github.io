67e6aaaeaf3dfe73f7acaf4b679318653d256912
kryan17 <74518901+kryan17@users.noreply.github.com>
2022-10-30 19:56:51 -0400

    Create mega_script.js

---
+++ MITV/scripts/mega_script.js
@@ -0,0 +1,17 @@
+       window.onLoad = function () {
+         var codeButtons = document.querySelectorAll('.code-unlock');
+         for (var codeButton : codeButtons)
+         {
+           var codeButtonUnlockDivId = codeButton.getAttribute('unlock-id');
+           var codeButtonHideDivId = codeButton.getAttribute('hide-id');
+           var codeButtonCode = codeButton.getAttribute('code');
+           var codeButtonInputId = codeButton.getAttribute('input-id');
+           codeButton.addEventListener('click', () => {
+              if (document.getElementById(codeButtonInputId).value == codeButtonCode)
+              {
+                document.getElementById(codeButtonUnlockDivId).hidden = false;
+                document.getElementById(codeButtonHideDivId).hidden = true;
+              }
+           });
+         }
+        };
